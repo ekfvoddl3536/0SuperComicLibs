64c2fdeef46a51e1585581d5a38e27a2e89c8f87
SuperComic <42625666+ekfvoddl3536@users.noreply.github.com>
2019-12-17 14:00:29 +0900

    Add files via upload

---
+++ 0SuperComicLib.LowLevel/Base/NativeStruct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SuperComicLib.LowLevel
+{
+    public unsafe sealed class NativeStruct<T> : PointerMethods<T> where T : struct
+    {
+        private UnsafeUnboxStruct<T> unbox;
+        private UnsafeReadPointerStruct<T> read;
+
+        public NativeStruct()
+        {
+            Type owner = typeof(NativeStruct<T>);
+
+            unbox = NativeClass.CreateUnbox<T>(owner);
+            read = NativeClass.CreateReadPointer<T>(owner);
+        }
+
+        public T Unbox(object obj) => unbox.Invoke(obj);
+
+        public T Read(void* ptr) => read.Invoke(ptr);
+
+        public override T Default(void* ptr) => Read(ptr);
+
+        protected override void Dispose(bool disposing)
+        {
+            unbox = null;
+            read = null;
+        }
+    }
+}
