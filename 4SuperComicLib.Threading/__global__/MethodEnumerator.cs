9b22bf151a3e8196e414c8c7f31bd41f9377b693
SuperComic <ekfvoddl3535@naver.com>
2021-02-02 07:42:34 +0900

    Add files via upload

---
+++ 4SuperComicLib.Threading/__global__/MethodEnumerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+namespace SuperComicLib.Threading
+{
+    internal sealed class MethodEnumerator : IDisposable, IEnumerator, IWorker
+    {
+        private AutoResetEvent mre;
+        private Action act;
+        private bool working;
+        private bool skipOne;
+
+        public MethodEnumerator()
+        {
+            mre = new AutoResetEvent(false);
+            act = null;
+            working = skipOne = true;
+        }
+
+        public object Current => null;
+
+        public Awaiter Invoke(Action action)
+        {
+            act = action;
+            skipOne = false;
+            return new Awaiter(mre);
+        }
+
+        public bool MoveNext()
+        {
+            if (working)
+            {
+                if (!skipOne)
+                {
+                    skipOne = true;
+                    act.Invoke();
+                    mre.Set();
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset() { }
+
+        public void Dispose()
+        {
+            if (mre != null)
+            {
+                mre.Dispose();
+                mre = null;
+
+                act = null;
+                working = skipOne = false;
+            }
+            GC.SuppressFinalize(this);
+        }
+    }
+}
