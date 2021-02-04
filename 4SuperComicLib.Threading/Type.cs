ee421025c0621b96f3af3e682fe843b5b88f5c70
SuperComic <ekfvoddl3535@naver.com>
2021-02-04 21:28:15 +0900

    Create Type.cs

---
+++ 4SuperComicLib.Threading/Type.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SuperComicLib.Threading
+{
+    public interface IWorker
+    {
+        Awaiter Invoke(Action action);
+    }
+
+    public interface IMultiWorker
+    {
+        Awaiter Invoke(int handle, Action action);
+    }
+}
