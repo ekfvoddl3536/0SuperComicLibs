49aa015b4c76822a7f45d43fe398d43d1b9e4fc5
SuperComic <ekfvoddl3535@naver.com>
2021-07-06 19:06:08 +0900

    Update prepare

--- 0SuperComicLib.Core/Public/__global__/extension/SCL_GLOBAL_ListExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SuperComicLib
-{
-    public static class SCL_GLOBAL_ListExtension
-    {
-        public static string Convert2Str(this List<char> vs) => new string(vs.ToArray());
-    }
-}
--- 0SuperComicLib.Core/__global/SUPERCOMICLIB_CORE__BooleanExtension__.cs
+++
@@ -1,5 +0,0 @@
-﻿public static class SUPERCOMICLIB_CORE__BooleanExtension__
-{
-    public static int ToInt(this bool value) => 
-        SuperComicLib.Core.Bool2Int._func(value);
-}
