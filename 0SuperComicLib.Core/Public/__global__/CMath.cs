ec344ca203ff51d313c78082914cbf334fbb624f
SuperComic <ekfvoddl3535@naver.com>
2022-01-26 18:26:16 +0900

    Update CMath.cs

--- 0SuperComicLib.Core/Public/__global__/CMath.cs
+++ 0SuperComicLib.Core/Public/__global__/CMath.cs
@@ -1,60 +1,78 @@
-﻿namespace SuperComicLib
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace SuperComicLib
 {
     public static class CMath
     {
-        public static int Min(this int left, int right) => left < right ? left : right;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Min(this int left, int right)
+        {
+            int temp = left - right;
+            return right + (temp & (temp >> 31));
+        }
 
-        public static int Max(this int left, int right) => left > right ? left : right;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Max(this int left, int right)
+        {
+            int temp = left - right;
+            return left - (temp & (temp >> 31));
+        }
 
-        public static int RngOut(this int num, int min, int max) =>
-            num < min
-            ? num
-            : num > max
-            ? num
-            : min;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int RngIn(this int num, int min, int max) => Max(min, Min(max, num));
 
-        public static int RngIn(this int num, int min, int max) =>
-            num > max
-            ? max
-            : num < min
-            ? min
-            : num;
-
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsRngIn(this int num, int min, int max) => num >= min && num <= max;
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsRngOut(this int num, int min, int max) => num < min || num > max;
 
         /// <summary>
         /// 양수부분만 남도록 강제로 자릅니다
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Pos1(this int value) => value & int.MaxValue;
 
         /// <summary>
         /// 강제로 음수로 설정합니다.
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Neg1(this int value) => value | int.MinValue;
 
-        public static int Abs(this int value) => value < 0 ? -value : value;
-        
-        // - slower than div 10
-        // public static int Div10fs(this int value) => (int)((value * 1717986919L) >> 34) - (value >> 31);
-        // 
-        // public static uint Div10fs(this uint value) => (uint)Div10fs((int)value);
-        // 
-        // public static int DivMod10fs(this int value, out int mod)
-        // {
-        //     int div = Div10fs(value);
-        //     mod = value - ((div << 2) + div << 1);
-        // 
-        //     return div;
-        // }
-        // 
-        // public static uint DivMod10fs(this uint value, out uint mod)
-        // {
-        //     uint div = (uint)Div10fs((int)value);
-        //     mod = value - ((div << 2) + div << 1);
-        // 
-        //     return div;
-        // }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Abs(this int value)
+        {
+            int temp = value >> 31;
+            return (value ^ temp) - temp;
+        }
+
+        // https://blog.naver.com/ekfvoddl3535/222607247076
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Normal(this int value) => (value >> 31) | (int)((uint)-value >> 31);
+
+        // https://blog.naver.com/ekfvoddl3535/222629296802
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Flip(this int value, int max_include) => value - ((value << 1) - max_include);
+
+        // https://blog.naver.com/ekfvoddl3535/222629296802
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Flip(this int value, int min_include, int max_include) => min_include + value - ((value << 1) - max_include);
+
+        // https://blog.naver.com/ekfvoddl3535/222629296802
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Flip_s(this int value, int max_include, int state)
+        {
+            Contract.Requires(state >= 0 && state <= 1, "invalid state value");
+            return value - ((value << 1) - max_include) * state;
+        }
+
+        // https://blog.naver.com/ekfvoddl3535/222629296802
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Flip_s(this int value, int min_include, int max_include, int state)
+        {
+            Contract.Requires(state >= 0 && state <= 1, "invalid state value");
+            return min_include + value - ((value << 1) - max_include) * state;
+        }
     }
 }
